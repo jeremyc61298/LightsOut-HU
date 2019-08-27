9d52b0080bcda11fa7386a7e57d0c3d110b861ff
Jeremy Campbell <JCAMPBELL2@harding.edu>
2019-08-27 18:47:12 -0500

    Init repo

---
+++ LightsOut/AboutForm.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace LightsOut
+{
+    partial class AboutForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AboutForm));
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.authorLabel = new System.Windows.Forms.Label();
+            this.descriptionLabel = new System.Windows.Forms.Label();
+            this.lightbulb = new System.Windows.Forms.PictureBox();
+            this.iconLabel = new System.Windows.Forms.Label();
+            this.okButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.lightbulb)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.ForeColor = System.Drawing.Color.Firebrick;
+            this.titleLabel.Location = new System.Drawing.Point(122, 25);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(186, 38);
+            this.titleLabel.TabIndex = 0;
+            this.titleLabel.Text = "Lights Out!";
+            // 
+            // authorLabel
+            // 
+            this.authorLabel.AutoSize = true;
+            this.authorLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.authorLabel.Location = new System.Drawing.Point(125, 73);
+            this.authorLabel.Name = "authorLabel";
+            this.authorLabel.Size = new System.Drawing.Size(164, 20);
+            this.authorLabel.TabIndex = 1;
+            this.authorLabel.Text = "By Jeremy Campbell";
+            // 
+            // descriptionLabel
+            // 
+            this.descriptionLabel.AutoSize = true;
+            this.descriptionLabel.Location = new System.Drawing.Point(68, 109);
+            this.descriptionLabel.Name = "descriptionLabel";
+            this.descriptionLabel.Size = new System.Drawing.Size(290, 17);
+            this.descriptionLabel.TabIndex = 2;
+            this.descriptionLabel.Text = "Turn off all the lights (black) to win the game!";
+            // 
+            // lightbulb
+            // 
+            this.lightbulb.Image = ((System.Drawing.Image)(resources.GetObject("lightbulb.Image")));
+            this.lightbulb.Location = new System.Drawing.Point(84, 25);
+            this.lightbulb.Name = "lightbulb";
+            this.lightbulb.Size = new System.Drawing.Size(42, 39);
+            this.lightbulb.TabIndex = 3;
+            this.lightbulb.TabStop = false;
+            // 
+            // iconLabel
+            // 
+            this.iconLabel.AutoSize = true;
+            this.iconLabel.Location = new System.Drawing.Point(49, 126);
+            this.iconLabel.Name = "iconLabel";
+            this.iconLabel.Size = new System.Drawing.Size(325, 17);
+            this.iconLabel.TabIndex = 4;
+            this.iconLabel.Text = "Icon: https://www.flaticon.com/authors/smashicons ";
+            // 
+            // okButton
+            // 
+            this.okButton.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.okButton.Location = new System.Drawing.Point(164, 152);
+            this.okButton.Name = "okButton";
+            this.okButton.Size = new System.Drawing.Size(75, 27);
+            this.okButton.TabIndex = 5;
+            this.okButton.Text = "OK";
+            this.okButton.UseVisualStyleBackColor = true;
+            // 
+            // AboutForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(410, 203);
+            this.Controls.Add(this.okButton);
+            this.Controls.Add(this.iconLabel);
+            this.Controls.Add(this.lightbulb);
+            this.Controls.Add(this.descriptionLabel);
+            this.Controls.Add(this.authorLabel);
+            this.Controls.Add(this.titleLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "AboutForm";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "About";
+            ((System.ComponentModel.ISupportInitialize)(this.lightbulb)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Label authorLabel;
+        private System.Windows.Forms.Label descriptionLabel;
+        private System.Windows.Forms.PictureBox lightbulb;
+        private System.Windows.Forms.Label iconLabel;
+        private System.Windows.Forms.Button okButton;
+    }
+}
---
+++ LightsOut/MainForm.cs
@@ -0,0 +1,137 @@
+﻿using System.Windows.Forms;
+using System;
+using System.Drawing;
+
+namespace LightsOut
+{
+    public partial class MainForm : Form
+    {
+        private const int GridOffset = 25;
+        private const int GridLength = 200;
+        private const int NumCells = 3;
+        private const int CellLength = GridLength / NumCells;
+
+        private bool[,] grid;
+        private Random rand;
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            rand = new Random();
+            grid = new bool[NumCells, NumCells];
+
+            for (int r = 0; r < NumCells; r++)
+                for (int c = 0; c < NumCells; c++)
+                    grid[r, c] = true;
+        }
+
+        private void MainForm_Load(object sender, System.EventArgs e)
+        {
+
+        }
+
+        private void GameToolStripMenuItem_Click(object sender, System.EventArgs e)
+        {
+
+        }
+
+        private void newGameButton_Click(object sender, System.EventArgs e)
+        {
+            // Fill grid with either white or black
+            for (int r = 0; r < NumCells; r++)
+                for (int c = 0; c < NumCells; c++)
+                    grid[r, c] = rand.Next(2) == 1;
+
+            // Redraw grid
+            this.Invalidate();
+
+        }
+
+        private void MainForm_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+
+            for (int r = 0; r < NumCells; r++)
+            {
+                for (int c = 0; c < NumCells; c++)
+                {
+                    // Get proper pen and brush for on/off grid section
+                    Brush brush;
+                    Pen pen;
+                    
+                    if (grid[r, c])
+                    {
+                        pen = Pens.Black;
+                        brush = Brushes.White;
+                    }
+                    else
+                    {
+                        pen = Pens.White;
+                        brush = Brushes.Black;
+                    }
+
+                    int x = c * CellLength + GridOffset;
+                    int y = r * CellLength + GridOffset;
+
+                    g.DrawRectangle(pen, x, y, CellLength, CellLength);
+                    g.FillRectangle(brush, x + 1, y + 1, CellLength - 1, CellLength - 1);
+                }
+            }
+        }
+
+        private void MainForm_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.X < GridOffset || e.X > CellLength * NumCells + GridOffset ||
+                e.Y < GridOffset || e.Y > CellLength * NumCells + GridOffset)
+                return;
+
+            int r = (e.Y - GridOffset) / CellLength;
+            int c = (e.X - GridOffset) / CellLength;
+
+            for (int i = r - 1; i <= r + 1; i++)
+                for (int j = c - 1; j <= c + 1; j++)
+                    if (i >= 0 && i < NumCells && j >= 0 && j < NumCells)
+                        grid[i, j] = !grid[i, j];
+
+            this.Invalidate();
+
+            if (PlayerWon())
+            {
+                MessageBox.Show(this, "Congratulations! You've Won!", "Lights Out!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+        
+        private bool PlayerWon()
+        {
+            for (int r = 0; r < NumCells; r++)
+                for (int c = 0; c < NumCells; c++)
+                    if (grid[r, c])
+                        return false;
+
+            return true;
+        }
+
+        private void NewToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.newGameButton_Click(sender, e);
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void ExitToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            ExitButton_Click(sender, e);
+        }
+
+        private void AboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AboutForm aboutBox = new AboutForm();
+            aboutBox.ShowDialog(this);
+        }
+    }
+}
+
